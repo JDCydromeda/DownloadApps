dc82fdcde0993bfa08f65e96995b8108e20f4ace
JDCydromeda <ceo@cydromeda.cf>
2021-06-18 14:27:59 +0530

    Update urls.py

--- DownloadApps/urls.py
+++ DownloadApps/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from download.urls import *
+from django.conf import settings
+from django.conf.urls.static import static
+import 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
