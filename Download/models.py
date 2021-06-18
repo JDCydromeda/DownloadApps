7e55fad8a1b3af020a660c21a0bb0602e479807e
JDCydromeda <ceo@cydromeda.cf>
2021-06-18 19:19:52 +0530

    Updated v1.0

--- Download/models.py
+++ Download/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Apps(models.Model):
+    Name = models.CharField(max_length=20)
+    Image = models.FileField(upload_to="images")
+    Description = models.CharField(max_length=500)
+    Download = models.FileField(upload_to="apps")
+
+    def __str__(self):
+        return self.Name
--- Download/views.py
+++ Download/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Apps
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    company_name = "Swoop"
+    title = "Download Swoop Apps"
+    company_email = "ceo@cydromeda.cf"
+    return render(request, 'index.html', {'apps': Apps.objects.all(), 'company_name': company_name, 'company_email': company_email, 'title': title})
+    
+
