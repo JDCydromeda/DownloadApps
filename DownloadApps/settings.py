53557efaaa9ca68cd4864c79801bda44d0b72dd4
JDCydromeda <ceo@cydromeda.cf>
2021-06-19 16:57:32 +0530

    Development v1.0.3

--- DownloadApps/settings.py
+++ DownloadApps/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -125,6 +125,9 @@ STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
